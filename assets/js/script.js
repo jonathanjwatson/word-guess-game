f2f9ceefd827a34c1e0fb87f845059dfc1677805
Jonathan J. Watson <jonathanwatson1@gmail.com>
2021-03-22 14:19:40 -0400

    added event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,25 @@ var secondsRemainingEl = document.getElementById("seconds-remaining");
 
 var wordToGuess = "banana";
 var wordToDisplay = "";
+var guessedLetters = [];
+
+var hasGameStarted = false;
+var wins = localStorage.getItem("wins");
+var losses = localStorage.getItem("losses");
 
 // JAVASCRIPT VARIABLES
 
 // FUNCTION DEFINITIONS
 
 function generateText() {
+  wordToDisplay = "";
   for (var i = 0; i < wordToGuess.length; i++) {
+    if (guessedLetters.includes(wordToGuess[i])) {
+      wordToDisplay = wordToDisplay + " " + wordToGuess[i] + " ";
+    } else {
+      wordToDisplay = wordToDisplay + " _ ";
+    }
     // Lookup the .includes method in javascript and think, "Where could I store, a user's guesses?"
-    // wordToDisplay = wordToDisplay + " " + wordToGuess[i] + " ";
-    wordToDisplay = wordToDisplay + " _ ";
   }
   wordToGuessEl.textContent = wordToDisplay;
 }
@@ -24,8 +33,19 @@ function generateText() {
 // EVENT LISTENERS
 
 // Listen for the start button click.
+startButton.addEventListener("click", function () {
+  hasGameStarted = true;
+  // Start the countdown
+});
 
 // Listen for keyboard presses.
+document.addEventListener("keydown", function (event) {
+  if (hasGameStarted) {
+    console.log(event.key);
+    guessedLetters.push(event.key);
+    generateText();
+  }
+});
 
 // FUNCTION CALLS
 generateText();
